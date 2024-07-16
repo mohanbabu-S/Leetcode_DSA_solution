0dc492f3554737cf06dddff7ec9dbe1071325671
S Mohan Babu <145913283+mohanbabu-S@users.noreply.github.com>
2024-07-16 15:11:14 +0530

    Time: 76 ms (93.73%), Space: 17.4 MB (28.7%) - LeetHub

---
+++ 0794-swim-in-rising-water/0794-swim-in-rising-water.py
@@ -0,0 +1,24 @@
+class Solution:
+    def swimInWater(self, grid: List[List[int]]) -> int:
+        Num = len(grid)
+        visit = set()
+        minH = [[grid[0][0], 0, 0]]  # (time/max-height, r, c)
+        directions = [[0, 1], [0, -1], [1, 0], [-1, 0]]
+
+        visit.add((0, 0))
+        while minH:
+            t, r, c = heapq.heappop(minH)
+            if r == Num - 1 and c == Num - 1:
+                return t
+            for dr, dc in directions:
+                neiR, neiC = r + dr, c + dc
+                if (
+                    neiR < 0
+                    or neiC < 0
+                    or neiR == Num
+                    or neiC == Num
+                    or (neiR, neiC) in visit
+                ):
+                    continue
+                visit.add((neiR, neiC))
+                heapq.heappush(minH, [max(t, grid[neiR][neiC]), neiR, neiC])
