c684bf857dfe4f829611b300987379736ac99f6a
S Mohan Babu <145913283+mohanbabu-S@users.noreply.github.com>
2024-07-23 20:26:11 +0530

    Time: 274 ms (27.57%), Space: 115.5 MB (5.22%) - LeetHub

---
+++ 0650-2-keys-keyboard/0650-2-keys-keyboard.py
@@ -0,0 +1,10 @@
+class Solution:
+    def minSteps(self, n: int) -> int:
+        @lru_cache(None)
+        def dp(ops, curr, saved):
+            if curr == n: return ops
+            if curr > n: return inf
+            if curr == saved: return dp(ops + 1, curr + saved, saved)
+            if saved == 0: return dp(ops + 1, curr, curr)
+            return min(dp(ops + 1, curr + saved, saved), dp(ops + 1, curr, curr))
+        return dp(0, 1, 0)    
